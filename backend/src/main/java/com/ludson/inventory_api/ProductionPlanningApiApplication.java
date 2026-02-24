6db66dabc6c450c5e3f3b138e5318daa1921180b
ludson96 <ludson_ps27@hotmail.com>
2026-02-24 14:26:10 -0300

    feat: :tada: initiate backend in java

---
+++ backend/src/main/java/com/ludson/inventory_api/ProductionPlanningApiApplication.java
@@ -0,0 +1,13 @@
+package com.ludson.inventory_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionPlanningApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionPlanningApiApplication.class, args);
+	}
+
+}
